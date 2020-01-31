41f03575b5fa1661dd699b2fde88cd2959c5e223
FrankyDevelop <flunac567@gmail.com>
2020-01-30 22:38:51 -0500

    Codigo de produccion

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,9 @@
 const mongoose=require('mongoose');
 
+
 mongoose.connect(process.env.MONGODB_URI,{
-    useUnifiedTopology:true,
-    useNewUrlParser:true
+    useNewUrlParser:true,
+    useUnifiedTopology:true
 })
 .then(db=>console.log("db conectada"))
 .catch(err=>console.log(err));
